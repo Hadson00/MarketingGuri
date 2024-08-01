a61c1cd14cf7358ed7decd7b9f374914d70e0d21
Hadson00 <hadson798@gmail.com>
2024-08-01 12:55:08 -0300

    sla

--- MyProject/myapp/views.py
+++ MyProject/myapp/views.py
@@ -4,7 +4,6 @@ from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-
 @login_required
 def home(request):
     card = Card.objects.all()
