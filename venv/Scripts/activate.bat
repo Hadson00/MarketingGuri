ac840c7be2ea458d4ee89a94d175a64b956d3bfd
Hadson00 <hadson798@gmail.com>
2024-08-20 21:25:06 -0300

    Arquivo md

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Hadson\OneDrive\Documentos\MarketingGuri\venv
+set VIRTUAL_ENV=C:\Users\Trilhas03\Desktop\MarketingGuri\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
